3056fc81f67f25750b76b446bc2cc6033e4f9bc5
Freek Van der Herten <freek@spatie.be>
2025-05-16 11:57:22 +0200

    style form

--- resources/lang/en/form.php
+++ resources/lang/en/form.php
@@ -1,8 +1,27 @@
 <?php
 
 return [
-    'title' => 'One time login code',
-    'password_label' => 'Enter your login code',
-    'submit_button' => 'Verify',
-    'invalid_password' => 'The login code you entered is invalid. Please try again.',
+    'email_form_title' => 'Login with One-Time Password',
+    'email_label' => 'E-mail',
+    'send_login_code_button' => 'Send one-time login code',
+
+    'one_time_password_form_title' => 'Enter your login code',
+    'password_label' => 'Login code',
+    'submit_login_code_button' => 'Submit one-time login code',
 ];
+
+
+/*
+'title' => 'One time login code',
+        'password_label' => 'Enter your login code',
+        'submit_button' => 'Verify',
+        'invalid_password' => 'The login code you entered is invalid. Please try again.',
+*/
+
+/*
+'one_time_password_form' => [
+    'title' => 'Login with One-Time Password',
+    'email_label' => 'Email',
+    'button_text' => 'Send one-time login code',
+],
+*/
