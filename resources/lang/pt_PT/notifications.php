f654e493a7395d9fe1afbd9ac0c08fc38414ffa4
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-05-17 10:23:46 +0000

    Fix styling

--- resources/lang/pt_PT/notifications.php
+++ resources/lang/pt_PT/notifications.php
@@ -6,4 +6,3 @@ return [
     'intro' => 'Este é o seu código único de acesso para usar em :url:',
     'outro' => 'Para proteger a sua conta, não partilhe este código com ninguém. Se não solicitou este código, pode ignorar este e-mail com segurança.',
 ];
-
